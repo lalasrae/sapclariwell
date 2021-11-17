6a7420343b5730118da5a5738969a8948c90736c
NILESH_PC <lalsarenilesh222@gmail.com>
2021-11-17 18:50:43 +0530

    added port

--- clariwelltech/settings.py
+++ clariwelltech/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = "clariwelltech@gmail.com"
 EMAIL_HOST_PASSWORD = "Clariwelltech@04"
 EMAIL_USE_TLS = True
